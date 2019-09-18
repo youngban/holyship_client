b86d465a0cd6d3e516c0e23fef22583b567818be
choisuyang <chltndid724@gmail.com>
2019-09-18 22:34:56 +0900

    Join-Login-Css

--- hollyship/App.tsx
+++ hollyship/App.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import StartScreen from './src/screens/StartScreen';
+// import StartScreen from './src/screens/StartScreen';
+import LoginScreen from './src/screens/LoginScreen';
+// import JoinScrzeen from './src/screens/JoinScreen';
 
 export default function App() {
-  return <View style={styles.container}>{<StartScreen />}</View>;
+  return <View style={styles.container}>{<LoginScreen />}</View>;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
