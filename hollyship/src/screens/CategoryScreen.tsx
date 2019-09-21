4a06bbcd43e30f5cd8e8f5d74946d4126eb3b626
young ban <ban1990@naver.com>
2019-09-21 19:28:52 +0900

    Category to Emotion

--- hollyship/src/screens/CategoryScreen.tsx
+++ hollyship/src/screens/CategoryScreen.tsx
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
+import { Icon } from 'react-native-elements';
 
 export default class CategoryScreen extends Component {
   render() {
-    return <Text>Category</Text>;
+    console.log(this.props);
+    return (
+      <View>
+        <Icon
+          name="sentiment-neutral"
+          type="MaterialIcons"
+          size={60}
+          onPress={() => this.props.navigation.navigate('EmotionScreen')}
+        >
+          <Text>happy</Text>
+        </Icon>
+      </View>
+    );
   }
 }
