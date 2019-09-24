58c510a1e9cbd51e5a216f5159020d30585a906b
young ban <ban1990@naver.com>
2019-09-24 13:55:44 +0900

    readPosting -ing

--- hollyship/src/screens/EmotionScreen.tsx
+++ hollyship/src/screens/EmotionScreen.tsx
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
+import { NavigationStackProp } from 'react-navigation-stack';
 
+type Props = {
+  navigation: NavigationStackProp<{ category: 'string' }>;
+};
 export default class EmotinScreen extends Component {
   static navigationOptions = ({ navigation }) => {
     return {
-      title: navigation.getParam('key'),
+      title: navigation.getParam('category'),
     };
   };
   render() {
-    const key = this.props.navigation.state.params.key;
+    const header = this.props.navigation.state.params.category;
 
-    return <Text>{key}</Text>;
+    return <Text>{header}</Text>;
   }
 }
