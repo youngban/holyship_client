97c69693c9f940cad5a8a667250da1f8d9bfe47f
geonhwiii <g01063962671@gmail.com>
2019-10-05 14:17:17 +0900

    Applying kiteen & login ui

--- hollyship/src/screens/LoginScreen.tsx
+++ hollyship/src/screens/LoginScreen.tsx
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import {
-  View,
-  Text,
-  TextInput,
   StyleSheet,
-  TouchableOpacity,
   Alert,
   AsyncStorage,
   KeyboardAvoidingView,
+  Dimensions,
+  TouchableWithoutFeedback,
+  Keyboard,
 } from 'react-native';
+import { Layout, Text, Input, Button } from 'react-native-ui-kitten';
 
 const axios = require('axios');
 
@@ -26,14 +26,10 @@ export default class LoginScreen extends Component<Props, State> {
   static navigationOptions = {
     header: 'Login',
   };
-
-  constructor(props: Props) {
-    super(props);
-    this.state = {
-      email: '',
-      password: '',
-    };
-  }
+  state = {
+    email: '',
+    password: '',
+  };
 
   handleLogin = async () => {
     try {
@@ -58,42 +54,80 @@ export default class LoginScreen extends Component<Props, State> {
     }
   };
 
+  // TODO: checked valid input
+  isValidInputEmail = () => {
+    const { email } = this.state;
+    return email.length > 0;
+  };
+
+  isValidInputPassword = () => {
+    const { password } = this.state;
+    return password.length >= 6;
+  };
+
   render() {
+    const isValidInputEmail = this.isValidInputEmail();
+    const isValidInputPassword = this.isValidInputPassword();
     return (
-      <KeyboardAvoidingView style={{ flex: 1 }} behavior="padding">
-        <View style={styles.container}>
-          <View style={styles.titleArea}>
-            <Text style={styles.title}>Holly Ship</Text>
-          </View>
-          <View style={styles.formArea}>
-            <TextInput
-              style={styles.textForm}
-              placeholder="ID"
-              // value={this.state.username}
-              onChangeText={text => this.setState({ email: text })}
-            />
-            <TextInput
-              style={styles.textForm}
-              secureTextEntry={true}
-              placeholder={'Password'}
-              // value={this.state.password}
-              onChangeText={text => this.setState({ password: text })}
-            />
-          </View>
-          <View style={styles.buttomArea}>
-            <TouchableOpacity
-              style={styles.button}
-              onPress={this.handleLogin}
-              // onPress={() => {
-              //   this.props.navigation.navigate('Home');
-              // }}
-            >
-              <Text style={styles.buttonTitle}>Login</Text>
-            </TouchableOpacity>
-          </View>
-          <View style={{ height: '10%' }}></View>
-        </View>
-      </KeyboardAvoidingView>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+        <KeyboardAvoidingView style={{ flex: 1 }} behavior="padding">
+          <Layout style={styles.container}>
+            <Layout style={styles.titleArea}>
+              <Text category="h1" style={styles.title}>
+                Holly Ship
+              </Text>
+            </Layout>
+
+            <Layout style={styles.formArea}>
+              <Input
+                style={styles.textInput}
+                status={isValidInputEmail ? 'info' : 'danger'}
+                caption={isValidInputEmail ? 'Correct' : '빈칸은 안되요!'}
+                placeholder="ID"
+                onChangeText={text => this.setState({ email: text })}
+              />
+              <Input
+                style={styles.textInput}
+                status={isValidInputPassword ? 'info' : 'danger'}
+                caption={
+                  isValidInputPassword
+                    ? 'Correct'
+                    : '비밀번호는 6자 이상이에요!'
+                }
+                keyboardType="decimal-pad"
+                secureTextEntry={true}
+                placeholder={'Password'}
+                // value={this.state.password}
+                onChangeText={text => this.setState({ password: text })}
+              />
+            </Layout>
+            <Layout style={styles.buttonWrapper}>
+              <Button
+                style={styles.loginButton}
+                status="primary"
+                appearance="outline"
+                size="large"
+                disabled={
+                  isValidInputEmail && isValidInputPassword ? false : true
+                }
+                onPress={this.handleLogin}
+              >
+                LOGIN
+              </Button>
+              <Button
+                style={styles.signupButton}
+                status="danger"
+                appearance="outline"
+                size="large"
+                onPress={this.handleLogin}
+              >
+                SIGNUP
+              </Button>
+            </Layout>
+            <Layout style={{ height: '10%' }}></Layout>
+          </Layout>
+        </KeyboardAvoidingView>
+      </TouchableWithoutFeedback>
     );
   }
 }
@@ -101,60 +135,38 @@ export default class LoginScreen extends Component<Props, State> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'black',
     paddingLeft: '10%',
     paddingRight: '10%',
     alignItems: 'center',
-    justifyContent: 'flex-end',
+    justifyContent: 'center',
   },
   titleArea: {
-    width: '100%',
-    padding: '10%',
-    alignItems: 'center',
-    paddingTop: '20%',
-    paddingBottom: '10%',
+    marginBottom: 30,
   },
   title: {
-    fontSize: 35,
-    color: 'ghostwhite',
     textShadowOffset: { width: 2, height: 2 },
     textShadowRadius: 20,
     textShadowColor: 'red',
   },
   formArea: {
     width: '100%',
-    // paddingBottom: '10%',
+    marginBottom: 5,
   },
-  textForm: {
-    width: '100%',
-    height: '20%',
-    paddingLeft: 15,
-    paddingRight: 5,
-    marginBottom: '10%',
+  textInput: {
+    marginTop: 10,
+    marginBottom: 10,
     borderRadius: 30,
-    color: 'white',
-    borderBottomWidth: 1,
     borderColor: 'plum',
   },
-  buttomArea: {
-    width: '50%',
-    height: '10%',
+  buttonWrapper: {
+    padding: 10,
   },
-  button: {
-    borderColor: 'plum',
-    borderWidth: 3,
-    borderRadius: 40,
-    width: '100%',
-    height: '100%',
-    justifyContent: 'center',
-    alignItems: 'center',
+  loginButton: {
+    margin: 10,
+    width: Dimensions.get('screen').width - 40,
   },
-  buttonTitle: {
-    color: 'ghostwhite',
-    fontSize: 20,
-    fontWeight: 'bold',
-    textShadowOffset: { width: 2, height: 2 },
-    textShadowRadius: 20,
-    textShadowColor: 'red',
+  signupButton: {
+    margin: 10,
+    width: Dimensions.get('screen').width - 40,
   },
 });
