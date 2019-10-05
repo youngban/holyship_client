32331eadabce112a26b5b9d2d921cf80957e59d5
geonhwiii <g01063962671@gmail.com>
2019-10-05 14:17:29 +0900

    Fix error

--- hollyship/App.tsx
+++ hollyship/App.tsx
@@ -1,14 +1,23 @@
 import * as React from 'react';
-import Appstack from './src/screens/index';
+import { mapping, dark as darkTheme } from '@eva-design/eva';
+import { ApplicationProvider, Layout } from 'react-native-ui-kitten';
+import AppStack from './src/screens/index';
 
 interface Props {}
 interface State {}
+
+const ApplicationContent = () => (
+  <Layout style={{ flex: 1, justifyContent: 'center' }}>
+    <AppStack />
+  </Layout>
+);
+
 export default class App extends React.Component<Props, State> {
   render() {
-    return <Appstack />;
+    return (
+      <ApplicationProvider mapping={mapping} theme={darkTheme}>
+        <ApplicationContent />
+      </ApplicationProvider>
+    );
   }
 }
-
-// export default function App() {
-//   return <Appstack />;
-// }
